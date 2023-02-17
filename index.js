7bf4b655338a63eff13fcb16ed71c5881f7a4a85
Pulkit0111 <ptyagi1551@gmail.com>
2023-02-17 21:24:53 +0530

    learning deployment

---
+++ index.js
@@ -0,0 +1,31 @@
+const express=require("express")
+const {connection}=require("./db")
+const {userRouter}=require("./routes/User.routes")
+const {noteRouter}=require("./routes/Note.route")
+const {authenticate}=require("./middlewares/authenticate.middleware")
+const cors=require("cors")
+require("dotenv").config()
+
+const app=express()
+
+app.use(express.json())
+app.use(cors())
+
+app.get("/",(req,res)=>{
+    res.send("HOME PAGE")
+})
+
+app.use("/users",userRouter)
+app.use(authenticate)
+app.use("/notes",noteRouter)
+
+
+app.listen(process.env.port,async ()=>{
+    try{
+        await connection
+        console.log("Connected to DB")
+    }catch(err){
+        console.log(err.message)
+    }
+    console.log(`Server is running at port ${process.env.port}`)
+})
